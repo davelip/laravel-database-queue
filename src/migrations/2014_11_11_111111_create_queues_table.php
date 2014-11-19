599a8b7499492c23cff97015c0131fa0a5660444
David Lippi <david.lippi@softecspa.it>
2014-11-19 12:15:39 +0100

    typo in migration filename

--- src/migrations/2014_11_11_111111_create_queues_table.php
+++ src/migrations/2014_11_11_111111_create_queues_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateQueueTable extends Migration {
+class CreateQueuesTable extends Migration {
 
 	/**
 	 * Run the migrations.
